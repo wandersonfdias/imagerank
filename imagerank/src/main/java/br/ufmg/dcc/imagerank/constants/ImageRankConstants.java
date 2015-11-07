b6960ff7fcac0347115e184e75b3350cacf46877
wandersonfdias <wandersonf.dias@gmail.com>
2015-11-07 17:39:07 -0200

    Image Rank.

---
+++ imagerank/src/main/java/br/ufmg/dcc/imagerank/constants/ImageRankConstants.java
@@ -0,0 +1,15 @@
+package br.ufmg.dcc.imagerank.constants;
+
+/**
+ * @author Wanderson Ferreira Dias - <code>wandersonf.dias@gmail.com</code>
+ */
+public interface ImageRankConstants
+{
+	String ARQUIVO_SAIDA = "saida.txt";
+	String FIELD_SEPARATOR = ",";
+	String LINE_SEPARATOR = "\n";
+	String FILENAME_SEPARATOR = ".";
+	int DESCRIPTOR_PRECISION_VALUE = 10;
+	Character PAIR_ID_SEPARATOR = '|';
+	String EXTENSAO_IMAGEM = "jpg";
+}
