78de4af788197e087f7e028dd6ddc0984e4b29a2
wandersonfdias <wandersonf.dias@gmail.com>
2015-11-07 17:23:41 -0200

    First Commit

---
+++ imagerank/src/main/java/br/ufmg/dcc/imagerank/App.java
@@ -0,0 +1,13 @@
+package br.ufmg.dcc.imagerank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
