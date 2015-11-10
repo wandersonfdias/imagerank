53e9f118fab4e7315239f5b55cfd63a0c1e557c8
wandersonfdias <wandersonf.dias@gmail.com>
2015-11-10 16:59:03 -0200

    Ajustes.

--- imagerank/src/main/java/resources/infrastructure/shell/gera_base_treino.sh
+++ imagerank/src/main/java/resources/infrastructure/shell/gera_base_treino.sh
@@ -2,9 +2,9 @@
 diretorioBase="$PWD/base_completa"
 diretorioImagens="imagens"
 diretorioDescritores="descritores"
-diretorioPares = "pares";
-diretorioLACDataset = "lac_dataset";
+diretorioPares="pares"
+diretorioLACDataset="lac_dataset"
 arquivoParesOriginal="$PWD/pares_originais/tag-classes-reduced.dat"
-totalParesGerar = 600000;
+totalParesGerar=600000
 
 java -cp imagerank-1.0.0-SNAPSHOT.jar br.ufmg.dcc.imagerank.main.ImageRankBatchProcessor $diretorioBase $diretorioImagens $diretorioDescritores $diretorioPares $diretorioLACDataset $arquivoParesOriginal $totalParesGerar
