668cdc2d5f291e8a36a812d66e2fd3cc7294c06e
wandersonfdias <wandersonf.dias@gmail.com>
2015-11-10 23:34:01 -0200

    Ajustes

--- imagerank/src/main/java/resources/infrastructure/shell/gera_base_treino.sh
+++ imagerank/src/main/java/resources/infrastructure/shell/gera_base_treino.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+arquivoLog="gera_base_treino.log"
 diretorioBase="$PWD/base_completa"
 diretorioImagens="imagens"
 diretorioDescritores="descritores"
@@ -7,4 +8,6 @@ diretorioLACDataset="lac_dataset"
 arquivoParesOriginal="$PWD/pares_originais/tag-classes-reduced.dat"
 totalParesGerar=600000
 
-java -cp imagerank-1.0.0-SNAPSHOT.jar br.ufmg.dcc.imagerank.main.ImageRankBatchProcessor $diretorioBase $diretorioImagens $diretorioDescritores $diretorioPares $diretorioLACDataset $arquivoParesOriginal $totalParesGerar
+java -Xmx2500M -Dfile.encoding=UTF-8 -cp imagerank-1.0.0-SNAPSHOT.jar br.ufmg.dcc.imagerank.main.ImageRankBatchProcessor $diretorioBase $diretorioImagens $diretorioDescritores $diretorioPares $diretorioLACDataset $arquivoParesOriginal $totalParesGerar 2&> $arquivoLog &
+
+echo -e "Acompannhe o log do arquivo '"$arquivoLog"'..."
--- imagerank/src/main/java/resources/infrastructure/shell/remove_imagens_sem_pares.sh
+++ imagerank/src/main/java/resources/infrastructure/shell/remove_imagens_sem_pares.sh
@@ -2,4 +2,4 @@
 diretorioBase="$PWD/base_completa"
 diretorioImagens="imagens"
 arquivoPares="$PWD/pares_originais/tag-classes-reduced.dat"
-java -cp imagerank-1.0.0-SNAPSHOT.jar br.ufmg.dcc.imagerank.pairs.util.NonPairImageDelete $diretorioBase $diretorioImagens $arquivoPares
+java -Dfile.encoding=UTF-8 -cp imagerank-1.0.0-SNAPSHOT.jar br.ufmg.dcc.imagerank.pairs.util.NonPairImageDelete $diretorioBase $diretorioImagens $arquivoPares
